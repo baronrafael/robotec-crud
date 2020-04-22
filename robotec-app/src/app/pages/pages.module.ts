2951095548730535b2b5d3c91a69f942cb60dac3
Rafael Baron <baronrafael@hotmail.com>
2020-04-22 15:12:41 -0400

    app: adding guards

--- robotec-app/src/app/pages/pages.module.ts
+++ robotec-app/src/app/pages/pages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [PagesComponent],
+  declarations: [PagesComponent, DashboardComponent],
   imports: [
     CommonModule,
     SharedModule,
---
+++ robotec-app/src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router){}
+  
+  canActivate(){
+    if(!localStorage.getItem('userLogged')){
+    this.router.navigate([''], { replaceUrl: true });
+    return false;
+    }
+    else{
+      return true;
+    }
+    
+  }
+  
+}
